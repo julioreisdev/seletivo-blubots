931f5a549068e7c7e3159cd785d8b4332ed2abce
julioreisdev <juliosteam0352@gmail.com>
2022-08-06 20:18:29 -0300

    Adiciona base do projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <>App</>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
